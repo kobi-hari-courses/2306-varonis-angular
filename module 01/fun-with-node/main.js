612964e693d9298996403f368985d633f954283d
Kobi Hari <kobi2294@yahoo.com>
2023-06-27 15:33:36 +0300

    npm and nodejs example

--- module 01/fun-with-node/main.js
+++ module 01/fun-with-node/main.js
@@ -1,6 +1,11 @@
-console.log('Hello World');
+var colors = require('colors');
+var math = require('mathjs');
+console.log('Hello World'.rainbow);
 
 var i = 10;
 var j = 20;
 var k = i + j;
+
+var s = math.atan(2);
 console.log('k = ', k);
+console.log('arctan(s) = ', s);
