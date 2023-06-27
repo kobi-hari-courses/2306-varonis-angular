b9dfca258203f23ff47b2a1dfd7d995d6501c2e8
Kobi Hari <kobi2294@yahoo.com>
2023-06-27 14:45:53 +0300

    end of javascript demo

--- module 01/fun-with-js/main.js
+++ module 01/fun-with-js/main.js
@@ -2,3 +2,19 @@ function max(a, b) {
     if (a > b) return a;
     return b;
 }
+
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+Person.prototype.log = function() {
+    console.log('Hi I am a person, my name is ' + this.firstName + ' ' + this.lastName);
+}
+
+
+var p = new Person('John', 'Smith');
+var p2 = new Person('Paul', 'Marks');
+
+console.log('p.log === p2.log', p.log === p2.log);
