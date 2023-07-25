a5888988b1e3aecf1b47b232779292ea6a4247c2
Kobi Hari <kobi2294@yahoo.com>
2023-07-25 12:04:27 +0300

    Subjects demo

--- module 06/projects/fun-with-rxjs/src/app/app.component.ts
+++ module 06/projects/fun-with-rxjs/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Observer, from, of, timer } from 'rxjs';
+import { BehaviorSubject, Observable, Observer, ReplaySubject, Subject, from, of, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -40,16 +40,71 @@ export class AppComponent {
           console.log('Magia Lesham!!!');
           observer.next(21)
         }, 10000);
-
-
     });
   }
+
+  createCustomSubject(): Observable<number> {
+    const subject = new Subject<number>();
+
+    subject.next(1);
+    subject.next(1);
+    setTimeout(() => subject.next(2), 1000);
+    setTimeout(() => subject.next(3), 2000);
+    setTimeout(() => subject.next(5), 3000);
+    setTimeout(() => subject.next(8), 4000);
+    setTimeout(() => subject.next(13), 5000);
+    setTimeout(() => subject.complete(), 8000);
+    setTimeout(() => {
+      console.log('Magia Lesham!!!');
+      subject.next(21)
+    }, 10000);
+
+    return subject;
+  }
+
+  createCustomBehaviorSubject(): Observable<number> {
+    const subject = new BehaviorSubject<number>(1);
+
+    subject.next(1);
+    setTimeout(() => subject.next(2), 1000);
+    setTimeout(() => subject.next(3), 2000);
+    setTimeout(() => subject.next(5), 3000);
+    setTimeout(() => subject.next(8), 4000);
+    setTimeout(() => subject.next(13), 5000);
+    setTimeout(() => subject.complete(), 8000);
+    setTimeout(() => {
+      console.log('Magia Lesham!!!');
+      subject.next(21)
+    }, 10000);
+
+    return subject;
+  }
+
+  createCustomReplaySubject(): Observable<number> {
+    const subject = new ReplaySubject<number>();
+    subject.next(1);
+    subject.next(1);
+    setTimeout(() => subject.next(2), 1000);
+    setTimeout(() => subject.next(3), 2000);
+    setTimeout(() => subject.next(5), 3000);
+    setTimeout(() => subject.next(8), 4000);
+    setTimeout(() => subject.next(13), 5000);
+    setTimeout(() => subject.complete(), 8000);
+    setTimeout(() => {
+      console.log('Magia Lesham!!!');
+      subject.next(21)
+    }, 10000);
+
+    return subject;
+  }
+
 
   go() {
     const observerA = this.createObserver('A');
     const observerB = this.createObserver('B');
+    const observerC = this.createObserver('C');
 
-    const observable = this.createCustomObservable();
+    const observable = this.createCustomReplaySubject();
     console.log('Subscribing A');
     observable.subscribe(observerA);
 
@@ -57,6 +112,11 @@ export class AppComponent {
       console.log('Subscrbing B');
       observable.subscribe(observerB)
     }, 2500);
+
+    setTimeout(() => {
+      console.log('Subscrbing C');
+      observable.subscribe(observerC)
+    }, 12500);
 
   }
 }
