65683e1e55b720c79f249162d5a297f9cc8de714
Kobi Hari <kobi2294@yahoo.com>
2023-08-03 13:31:01 +0300

    removed pre-version 16 examples. All examples now use actionGroups and Features

--- module 12/projects/redux-quiz/src/app/redux/quiz.actions.ts
+++ module 12/projects/redux-quiz/src/app/redux/quiz.actions.ts
@@ -1,12 +1,4 @@
-// import { createAction, props } from "@ngrx/store";
-
 import { createActionGroup, emptyProps, props } from "@ngrx/store";
-
-// export const reset = createAction('[User Quiz] reset');
-
-// export const answerCurrentQuestion = createAction('[User Quiz] answer current question', 
-//     props<{answerIndex: number}>());
-
 
 export const userQuizActions  = createActionGroup({
     source: 'User Quiz', 
