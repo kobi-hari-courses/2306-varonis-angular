d4d6dd0e3a3829534394c23159b627fd46400f62
Kobi Hari <kobi2294@yahoo.com>
2023-08-06 11:25:22 +0300

    skeleton for the new router demo

---
+++ module 14/README.md
@@ -0,0 +1,1 @@
+# Routing in angular 16
---
+++ module 14/projects/fun-with-routing/src/app/pages/page-c/page-c.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-page-c',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-c.component.html',
+  styleUrls: ['./page-c.component.css']
+})
+export class PageCComponent {
+
+}
